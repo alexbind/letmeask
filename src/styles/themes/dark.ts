14a8e1f21a37ac76ce6b0648a6865ed187c823b8
Alexander <alexandersilva436@gmail.com>
2021-08-04 12:52:30 -0300

    letmeask

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'dark',
+    colors: {
+        primary: '#003049',
+        secundary: '#fca311',
+        background: '#3d405b',
+        text: '#333'
+    }
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'light',
+    colors: {
+        primary: '#03045E',
+        secundary: '#0077B6',
+        background: '#F5F5F5',
+        text: '#333'
+    }
+};
